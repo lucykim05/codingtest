2667c6ff1d636f273cc676f84d4cb0c01402c96f
lucykim05 <effelt0522@naver.com>
2025-09-25 23:46:56 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let count = 0;
+    let num = 0;
+
+    while (count < n) {
+        num++;
+        if (num % 3 === 0 || String(num).includes('3')) continue;
+        count++;
+    }
+
+    return num;
+}
