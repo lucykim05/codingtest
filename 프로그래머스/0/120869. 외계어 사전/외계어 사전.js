36800e828d57fae49ddf46e843f4db04527dd666
lucykim05 <effelt0522@naver.com>
2025-10-25 19:52:23 +0900

    [level 0] Title: 외계어 사전, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -1,16 +1,11 @@
 function solution(spell, dic) {
-    for (let i = 0; i < dic.length; i++) {
-        let word = dic[i];
-        let isMatch = true;
-        for (let j = 0; j < spell.length; j++) {
-            if (!word.includes(spell[j])) {
-                isMatch = false;
-                break;
-            }
-        }
-        if (isMatch && word.length === spell.length) {
-            return 1;
+    const a = spell.sort().join('')
+    
+    for(let word of dic) {
+        const sortedWord = [...word].sort().join('')
+        if(sortedWord === a) {
+            return 1
         }
     }
-    return 2;
+    return 2
 }
