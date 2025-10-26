7303e4c2efdf5aacd1e45b6f2a200fb485bc6c19
lucykim05 <effelt0522@naver.com>
2025-10-27 08:34:16 +0900

    [level 0] Title: 문자열 돌리기, Time: 45.22 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    const arr = str.split('')
+    for(let a of arr) {
+        console.log(a)
+    }
+});
