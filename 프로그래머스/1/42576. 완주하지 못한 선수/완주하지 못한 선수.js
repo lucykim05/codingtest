52e072a77a05640d1acd50798dec1400aa3b236d
lucykim05 <effelt0522@naver.com>
2026-01-19 22:59:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 61.73 ms, Memory: 64.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,14 @@
+function solution(participant, completion) {
+    const map = new Map()
+    for(const p of participant) {
+        map.set(p, (map.get(p) || 0 )+1)
+    }
+    
+    for (const c of completion) {
+        map.set(c, map.get(c) -1)
+    }
+    
+    for(const [a, b] of map) {
+        if(b!==0) return a
+    }
+}
